de2609ba3fd1433a35cf9a56d83e30185d851377
Ralf Kühnlein <11390695+acrork@users.noreply.github.com>
2019-01-21 13:06:44 +0100

    copyright

--- samples/config.js
+++ samples/config.js
@@ -1,6 +1,6 @@
 /*
  *
- * * Copyright 2015 Acrolinx GmbH
+ * * Copyright 2015-present Acrolinx GmbH
  * *
  * * Licensed under the Apache License, Version 2.0 (the "License");
  * * you may not use this file except in compliance with the License.
