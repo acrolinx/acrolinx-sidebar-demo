262764bc3e5f4c58ac879df5c75c55f3c3405a98
acromarco <Marco.Stahl@acrolinx.com>
2019-12-16 18:49:33 +0100

    DEV-18500 JS SDK Should Add Its Own Version Information to the About
    * Close webdriver after test

--- test/test.js
+++ test/test.js
@@ -5,13 +5,21 @@ const By = webdriver.By;
 const TIMEOUT_MS = 10000;
 
 describe("live demo", () => {
+  let driver;
   jest.setTimeout(TIMEOUT_MS);
 
-  const driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(new chrome.Options().headless())
-    .build();
-  driver.manage().setTimeouts({implicit: TIMEOUT_MS});
+  beforeEach(() => {
+    driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(new chrome.Options().headless())
+      .build();
+    driver.manage().setTimeouts({implicit: TIMEOUT_MS});
+  });
+
+  afterEach(() => {
+    driver.close();
+    driver.quit();
+  });
 
   it("displays Sidebar SDK JS version in the about-page of the start-page", async () => {
     driver.get('https://acrolinx.github.io/acrolinx-sidebar-demo/samples/single-editor.html');
